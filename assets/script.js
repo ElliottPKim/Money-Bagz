3911d9ae693ef4a08c437c9faca36fc04a3685cb
aditore <anthonyditore@gmail.com>
2022-01-29 12:41:56 -0800

    starting api framework

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,19 @@
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
+
+//news api string
+//GET https://newsapi.org/v2/everything?q=keyword&apiKey=75aa420ce6ff42f5a637c8295a616561
+
+//odds API
+var settings = {
+    "url": "https://v3.football.api-sports.io/{endpoint}",
+    "method": "GET",
+    "timeout": 0,
+    "headers": {
+      "x-rapidapi-key": "XxXxXxXxXxXxXxXxXxXxXxXx",
+      "x-rapidapi-host": "v3.football.api-sports.io"
+    },
+  };
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
