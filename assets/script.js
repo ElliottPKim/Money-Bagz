841ca9546c0d8672c309e7605fed79cebd842427
aditore <anthonyditore@gmail.com>
2022-02-01 21:44:36 -0800

    button

--- assets/script.js
+++ assets/script.js
@@ -6,31 +6,31 @@ $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
 //odds API
 
 //grab API
-function grabOddsApiBasketball() {
-    fetch("https://v1.basketball.api-sports.io/games?id=145", {
-	    "method": "GET",
-	    "headers": {
-		    "x-rapidapi-host": "v1.basketball.api-sports.io",
-		    "x-rapidapi-key": "46e87bccbba421563a6d9139daeba0cf"
-	    }
-    })
-    .then(function(response) {
-	    response.json().then(function (data) {
-            console.log(data);
+// function grabOddsApiBasketball() {
+//     fetch("https://v1.basketball.api-sports.io/games?id=145", {
+// 	    "method": "GET",
+// 	    "headers": {
+// 		    "x-rapidapi-host": "v1.basketball.api-sports.io",
+// 		    "x-rapidapi-key": "46e87bccbba421563a6d9139daeba0cf"
+// 	    }
+//     })
+//     .then(function(response) {
+// 	    response.json().then(function (data) {
+//             console.log(data);
 
-            var basketball = document.createElement('div');
-            basketball.textContent = `this sport is in: ${data.response[3]}`;
+//             var basketball = document.createElement('div');
+//             basketball.textContent = `this sport is in: ${data.response[3]}`;
 
-            $('#matchUps').append(basketball);
+//             $('#matchUps').append(basketball);
 
-            // grabOdds(data);
-        });
-    })
-    .catch(function (err) {
-        console.log(err);
-    });
+//             // grabOdds(data);
+//         });
+//     })
+//     .catch(function (err) {
+//         console.log(err);
+//     });
         
-}
+//}
 
 // function grabOdds(response) {
 
